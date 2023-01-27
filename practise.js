66dc7061fc859f4589c4559bcab0ea9c32ba8446
Moroz Stanislav <108823770+Morozik17@users.noreply.github.com>
2023-01-27 21:43:15 +0100

    commit js

---
+++ practise.js
@@ -0,0 +1,305 @@
+// Задача №1
+// Prompt и confirm
+// Спитати у користувача ім’я, пошту,  телефон
+
+// console.log(prompt("Введіть ім’я"));
+// console.log(prompt("Введіть пошту"));
+// console.log(prompt("Введіть телефон"));
+
+// 1.1
+// Prompt и confirm
+// Спитати у користувача імя та прізвище, адресу доставки, телефон
+
+// console.log(prompt("Введіть ім’я та прізвище"));
+// console.log(prompt("Введіть адресу доставки"));
+// console.log(prompt("Введіть телефон"));
+
+// 1.2
+// Prompt и confirm
+// Спитати у користувача автор книжки, назва, який формат електронний чи друковани
+
+// console.log(prompt("Автор книжки"));
+// console.log(prompt("Назва"));
+// console.log(prompt("формат"));
+
+// 1.3
+// Prompt и confirm
+// Спитати у користувача імя та прізвище, навчальний заклад де навчається
+
+// console.log(prompt("Автор книжки"));
+// console.log(prompt("Назва"));
+// console.log(prompt("формат"));
+
+// 1.4
+// Prompt и confirm
+// Спитати у користувача імя та прізвище, ким працює, де працює
+
+// console.log(prompt("Імя та прізвище"));
+// console.log(prompt("Ким працюєте"));
+// console.log(prompt("Де працюєте"));
+
+// 1.5
+// Prompt и confirm
+// Спитати у користувача імя та прізвище, адресу проживання, телефон
+
+// console.log(prompt("Імя та прізвище"));
+// console.log(prompt("Адреса проживання"));
+// console.log(prompt("Телефон"));
+
+/////////////////////////////////////////////////////////////////////////
+
+// Задача №2
+// Визначення типу значення, оператор typeof
+
+// const productName = 'Mango';
+// const pricePerItem = 150;
+// const isOnSale = false;
+
+// 2.1
+// Визначення типу значення, оператор typeof
+
+// const productName = typeof 'Ice cream';
+// const pricePerItem = typeof 70;
+// const isOnSale = typeof false;
+
+// console.log(productName);
+// console.log(pricePerItem);
+// console.log(isOnSale);
+
+// 2.2
+// Визначення типу значення, оператор typeof
+
+// const productName = 'Phone';
+// const pricePerItem = 15 000;
+// const isOnSale = true;
+
+// console.log(productName);
+// console.log(pricePerItem);
+// console.log(isOnSale);
+
+// 2.3
+// Визначення типу значення, оператор typeof
+
+// const productName = 'Bike';
+// const pricePerItem = 10 000;
+// const isOnSale = false;
+
+// console.log(productName);
+// console.log(pricePerItem);
+// console.log(isOnSale);
+
+// 2.4
+// Визначення типу значення, оператор typeof
+
+// const productName = 'Headphone';
+// const pricePerItem = 1 000;
+// const isOnSale = false;
+
+// console.log(productName);
+// console.log(pricePerItem);
+// console.log(isOnSale);
+
+// 2.5
+// Визначення типу значення, оператор typeof
+
+// const productName = 'Pizza';
+// const pricePerItem = 300;
+// const isOnSale = true;
+
+// console.log(productName);
+// console.log(pricePerItem);
+// console.log(isOnSale);
+
+//////////////////////////////////////////////////////////////
+
+// Задача №3
+
+// Оголоси дві змінні, productName для назви товару і pricePerItem для зберігання цни за штуку.
+// При оголошені присвой змінним наступні характеристики товара:
+
+// назва - строка "Droid"
+// ціна за штуку - число 2000
+
+// і вивести дані в консоль
+
+// 3.1
+// Оголоси дві змінні, productName для назви товару і pricePerItem для зберігання цни за штуку.
+// При оголошені присвой змінним наступні характеристики товара:
+
+// 1)назва - строка "Ice cream"
+// 2)ціна за штуку - число 70
+
+// 3)і вивести дані в консоль
+
+// const productName = "Ice cream";
+// const pricePerItem = 2000;
+
+// console.log("Назва товару:",productName);
+// console.log("Ціна товару:",pricePerItem);
+
+
+// 3.2
+// Оголоси дві змінні, productName для назви товару і pricePerItem для зберігання цни за штуку.
+// При оголошені присвой змінним наступні характеристики товара:
+
+// назва - строка "Phone"
+// ціна за штуку - число 15 000
+
+// і вивести дані в консоль
+
+// const productName = "Phone";
+// const pricePerItem = 15000;
+
+// console.log("Назва товару:",productName);
+// console.log("Ціна товару:",pricePerItem);
+
+// 3.3
+// Оголоси дві змінні, productName для назви товару і pricePerItem для зберігання цни за штуку.
+// При оголошені присвой змінним наступні характеристики товара:
+
+// назва - строка "Headphone"
+// ціна за штуку - число 1 000
+
+// і вивести дані в консоль
+
+// const productName = "Headphone";
+// const pricePerItem = 1 000;
+
+// console.log("Назва товару:",productName);
+// console.log("Ціна товару:",pricePerItem);
+
+// 3.4
+// Оголоси дві змінні, productName для назви товару і pricePerItem для зберігання цни за штуку.
+// При оголошені присвой змінним наступні характеристики товара:
+
+// назва - строка "Pizza"
+// ціна за штуку - число 300
+
+// і вивести дані в консоль
+
+// const productName = "Pizza";
+// const pricePerItem = 300;
+
+// console.log("Назва товару:",productName);
+// console.log("Ціна товару:",pricePerItem);
+
+// 3.5
+// Оголоси дві змінні, productName для назви товару і pricePerItem для зберігання цни за штуку.
+// При оголошені присвой змінним наступні характеристики товара:
+
+// назва - строка "T-shirt"
+// ціна за штуку - число 1 200
+
+// і вивести дані в консоль
+
+// const productName = "T-shirt";
+// const pricePerItem = 1200;
+
+// console.log("Назва товару:",productName);
+// console.log("Ціна товару:",pricePerItem);
+
+////////////////////////////////////////////////////////////////
+
+
+// Задача №4
+// Ім'я товару змінили на "Repair droid" і збільшили його ціну на 1500 кредитів.
+// Перевизнач значення змінних pricePerItem і productName після їх оголошення.
+
+// let productName = "Droid";
+// let pricePerItem = 2000;
+
+// 4.1
+// Ім'я товару змінили на "Ice cream cones" і збільшили його ціну на 150 кредитів.
+// Перевизнач значення змінних pricePerItem і productName після їх оголошення.
+
+// let productName = "Ice cream";
+// productName = "Ice cream cones";
+
+// console.log("Назва",productName);
+
+// let pricePerItem = 70;
+// pricePerItem = 220;
+
+// console.log("Ціна",pricePerItem);
+
+// 4.2
+// Ім'я товару змінили на "Motorola" і збільшили його ціну на 2500 кредитів.
+// Перевизнач значення змінних pricePerItem і productName після їх оголошення.
+
+// let productName = "Phone";
+// let pricePerItem = 15 000;
+
+// console.log("Назва",productName);
+
+// let pricePerItem = 15000;
+// pricePerItem = 17500;
+
+// console.log("Ціна",pricePerItem);
+
+// 4.3
+// Ім'я товару змінили на "AirProds" і збільшили його ціну на 500 кредитів.
+// Перевизнач значення змінних pricePerItem і productName після їх оголошення.
+
+// let productName = "Headphone";
+// let pricePerItem = 1 000;
+
+// console.log("Назва",productName);
+
+// let pricePerItem = 1000;
+// pricePerItem = 1500;
+
+// console.log("Ціна",pricePerItem);
+
+// 4.4
+// Ім'я товару змінили на "Hot Dog" і зменшили його ціну на 50 кредитів.
+// Перевизнач значення змінних pricePerItem і productName після їх оголошення.
+
+// let productName = "Pizza";
+// let pricePerItem = 300;
+
+// console.log("Назва",productName);
+
+// let pricePerItem = 300;
+// pricePerItem = 250;
+
+// console.log("Ціна",pricePerItem);
+
+// 4.5
+// Ім'я товару змінили на "Sweater" і збільшили його ціну на 500 кредитів.
+// Перевизнач значення змінних pricePerItem і productName після їх оголошення.
+
+// let productName = "T-shirt";
+// let pricePerItem = 1 200;
+
+// console.log("Назва",productName);
+
+// let pricePerItem = 1200;
+// pricePerItem = 1700;
+
+// console.log("Ціна",pricePerItem);
+
+///////////////////////////////////////////////////////////////////////////
+
+
+// Задача №5
+// Оголоси наступні змінні, використовуючи ключове слово const або let, 
+// і присвой їм відповідні значення.
+// Виведи дані в консоль.
+
+// topSpeed - число 160.
+// distance - число 617.54.
+// login - рядок "mango935".
+// isOnline - буль true.
+// isAdmin - буль false.
+
+// const topSpeed = 160;
+// const distance = 617.54;
+// const login = "mango935";
+// const isOnline = true;
+// const isAdmin = false;
+
+// console.log("Швидкість",topSpeed);
+// console.log("Дистанція",distance);
+// console.log("Логін",login);
+// console.log("В мережі",isOnline);
+// console.log("Адмін",isAdmin);
